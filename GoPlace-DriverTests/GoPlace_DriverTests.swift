73c9f47060d85cb4d1023d1072322a084728ea77
Marcelo <marcelogc28@hotmail.com>
2021-11-11 10:36:33 -0500

    Initial Commit

---
+++ GoPlace-DriverTests/GoPlace_DriverTests.swift
@@ -0,0 +1,33 @@
+//
+//  GoPlace_DriverTests.swift
+//  GoPlace-DriverTests
+//
+//  Created by dianna on 11/11/21.
+//
+
+import XCTest
+@testable import GoPlace_Driver
+
+class GoPlace_DriverTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
